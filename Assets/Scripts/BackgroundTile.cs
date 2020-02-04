a65ef01e4967ef3fcddb1387a0a03334c95aaf21
jiwonc-dev <jiwonc.dev@gmail.com>
2020-02-04 16:54:33 +0900

    add swipe and match

---
+++ Assets/Scripts/BackgroundTile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTile : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Initialize()
+    {
+        
+    }
+}
